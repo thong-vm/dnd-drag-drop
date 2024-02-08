b1f71a804463be991de28d3db354d4d76b5ff758
thong-vm <thongvm@sync-partners.com>
2024-02-08 11:56:29 +0900

    build(deps): update package.json, Card.js, Container.js and yarn.lock

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,53 @@
-function Card() {
-  return <></>;
+import { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
+const style = {
+  border: "1px dashed gray",
+  padding: "0.5rem 1rem",
+  marginBottom: ".5rem",
+  backgroundColor: "white",
+  cursor: "move",
+};
+function Card({ id, index, text, handleMoveCard }) {
+  const ref = useRef(null);
+
+  const [{ handlerId }, drop] = useDrop({
+    accept: "Card",
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      handleMoveCard(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "Card",
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
+  return (
+    <div style={{ ...style, opacity }} ref={ref} data-handler-id={handlerId}>
+      {text}
+    </div>
+  );
 }
 
 export default Card;
